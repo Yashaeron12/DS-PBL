772c688cf46adda380b776aa70b850b096698772
Yash aeron <240211215@geu.ac.in>
2025-10-28 13:52:09 +0530

    Add command evaluation and editing functions
    Added functions to evaluate and edit commands.

--- command_parser.h
+++ command_parser.h
@@ -1,8 +1,9 @@
 #ifndef COMMAND_PARSER_H
 #define COMMAND_PARSER_H
 
-
 char* parse_command(char* input);
-
+int is_eval_command(char* input);
+int is_editor_command(char* input);
+char* extract_eval_expression(char* input);
 
 #endif
