f1c25ece25d87f1208ce4c8fad30d6c5f4c84942
Yash aeron <240211215@geu.ac.in>
2025-10-28 15:33:39 +0530

    Refactor Stack structure in stack.h
    Updated Stack structure and reduced item array size.

--- stack.h
+++ stack.h
@@ -1,18 +1,15 @@
 #ifndef STACK_H
 #define STACK_H
 
-
 typedef struct {
-char items[256];
-int top;
+    char data[100];
+    int top;
 } Stack;
 
-
 void initStack(Stack* s);
-int isEmpty(Stack* s);
 void push(Stack* s, char c);
 char pop(Stack* s);
 char peek(Stack* s);
-
+int isEmpty(Stack* s);
 
 #endif
