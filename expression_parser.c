b91849d8ac06e026bf0d27500a0b909117493ed7
Yash aeron <240211215@geu.ac.in>
2025-10-28 15:31:44 +0530

    Enhance infix_to_postfix for better parsing
    Refactor infix_to_postfix to handle spaces, decimals, functions, and variables.

--- expression_parser.c
+++ expression_parser.c
@@ -1,44 +1,106 @@
 #include <ctype.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "expression_parser.h"
 #include "stack.h"
 #include "utils.h"
 
-
 void infix_to_postfix(char* infix, char* postfix) {
-Stack s;
-initStack(&s);
-int k = 0;
-
-
-for (int i = 0; infix[i]; i++) {
-char c = infix[i];
-if (isdigit(c)) {
-    while (isdigit(infix[i])) {
-        postfix[k++] = infix[i++];
+    Stack s;
+    initStack(&s);
+    
+    int j = 0;
+    int i = 0;
+    
+    while (infix[i]) {
+        char c = infix[i];
+        
+        if (isspace(c)) {
+            i++;
+            continue;
+        }
+        
+        // Handle numbers (including decimals)
+        if (isdigit(c) || c == '.') {
+            while (isdigit(infix[i]) || infix[i] == '.') {
+                postfix[j++] = infix[i++];
+            }
+            postfix[j++] = ' ';
+            continue;
+        }
+        
+        // Handle functions and variables
+        if (isalpha(c)) {
+            char token[32] = {0};
+            int k = 0;
+            
+            while (isalnum(infix[i]) || infix[i] == '_') {
+                token[k++] = infix[i++];
+            }
+            
+            if (is_function_name(token)) {
+                push(&s, 'f');
+                sprintf(postfix + j, "func_%s ", token);
+                j += strlen(postfix + j);
+            } else if (is_constant(token)) {
+                sprintf(postfix + j, "const_%s ", token);
+                j += strlen(postfix + j);
+            } else {
+                sprintf(postfix + j, "var_%s ", token);
+                j += strlen(postfix + j);
+            }
+            continue;
+        }
+        
+        // Handle operators and parentheses
+        switch (c) {
+            case '(':
+                push(&s, c);
+                break;
+                
+            case ')':
+                while (!isEmpty(&s) && peek(&s) != '(') {
+                    if (peek(&s) == 'f') {
+                        postfix[j++] = 'f';
+                        postfix[j++] = ' ';
+                        pop(&s);
+                    } else {
+                        postfix[j++] = pop(&s);
+                        postfix[j++] = ' ';
+                    }
+                }
+                if (!isEmpty(&s)) pop(&s);
+                break;
+                
+            case '!':
+                postfix[j++] = c;
+                postfix[j++] = ' ';
+                break;
+                
+            default:
+                if (is_operator(c)) {
+                    while (!isEmpty(&s) && peek(&s) != '(' && peek(&s) != 'f' &&
+                           precedence(peek(&s)) >= precedence(c)) {
+                        postfix[j++] = pop(&s);
+                        postfix[j++] = ' ';
+                    }
+                    push(&s, c);
+                }
+                break;
+        }
+        i++;
     }
-    postfix[k++] = ' '; 
-    i--; 
-}
-
-else if (c == '(') {
-push(&s, c);
-}
-else if (c == ')') {
-while (!isEmpty(&s) && peek(&s) != '(') {
-postfix[k++] = pop(&s);
-}
-pop(&s);
-}
-else if (isOperator(c)) {
-while (!isEmpty(&s) && precedence(peek(&s)) >= precedence(c)) {
-postfix[k++] = pop(&s);
-}
-push(&s, c);
-}
-}
-while (!isEmpty(&s)) {
-postfix[k++] = pop(&s);
-}
-postfix[k] = '\0';
+    
+    while (!isEmpty(&s)) {
+        char op = pop(&s);
+        if (op == 'f') {
+            postfix[j++] = 'f';
+        } else {
+            postfix[j++] = op;
+        }
+        postfix[j++] = ' ';
+    }
+    
+    postfix[j] = '\0';
 }
